88f7231a5b7caa16a9ad12d2d70d20d4955bbccd
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2020-10-17 19:44:46 +0530

    removed log files

--- src/logger.py
+++ src/logger.py
@@ -5,15 +5,18 @@ class Logger:
             self.filename='../logs/{0}_{1}.log'.format(filename,self.date)
             with open(filename,'a') as file:
                 file.write("logger initialized")
+                file.write('\n')
 
         def info(self,msg):
 
             with open(self.filename,'a')  as file:
                 statement='INFO:{0}:{1}'.format(datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S"),msg)
                 file.write(statement)
+                file.write('\n')
         def error(self,msg):
 
             with open(self.filename,'a')  as file:
                 statement='ERROR:{0}:{1}'.format(datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S"),msg)
                 file.write(statement)
+                file.write('\n')
         
