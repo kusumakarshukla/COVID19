fa8e9c8dc933b27b342385ae4bf56a09c0183f45
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2020-10-18 19:16:06 +0530

    Committing latest changes with modified history url

--- src/app.py
+++ src/app.py
@@ -5,13 +5,20 @@ import plotly.express as px
 import pandas as pd
 from covidCases import CovidCases
 import plotly.graph_objects as go
-
+import dash_daq as daq
 covid=CovidCases()
 regions=covid.latest_cases()
 summary=covid.summary()
 
-app = dash.Dash(__name__)
 
+external_stylesheets = ['https://codepen.io/anon/pen/mardKv.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+theme =  {
+    'dark': True,
+    'detail': '#007439',
+    'primary': '#00EA64',
+    'secondary': '#6E6E6E',
+}
 
 df = pd.DataFrame({
     "States": [i['loc'] for i in regions],
@@ -28,18 +35,24 @@ fig = go.Figure(data=[
     go.Bar(name='Discharged', x=df['States'], y=df['Discharged'])
     
 ])
-app.layout = html.Div(id='dark-theme-container',children=[
-    html.H1(children='Latest COVID Cases'),
+app.layout = html.Div(id='dark-theme-container',children=[daq.DarkThemeProvider(theme=theme),
+    html.H1(children='Latest Covid-19 Cases'),
 
     
     html.Div([
         html.Table([
             html.Tr(
-                [html.Td(html.H1('Total')),html.Td(html.H1('Overall Confirmed')),html.Td(html.H1('Overall Discharged')),html.Td(html.H1('Overall Deaths'))]
+                [html.Td(html.H4('Total')),html.Td(html.H4('Overall Confirmed')),html.Td(html.H4('Overall Discharged')),html.Td(html.H4('Overall Deaths'))]
 
             ),
             html.Tr(
-                [html.Td(html.H1(summary['total'])),html.Td(html.H1(summary['confirmedCasesIndian'])),html.Td(html.H1(summary['discharged'])),html.Td(html.H1(summary['deaths']))]
+                [html.Td(daq.LEDDisplay(
+        value="3.14159",
+        color=theme['primary'],
+        id='darktheme-daq-leddisplay',
+        className='dark-theme-control'
+        
+    )),html.Td(html.H4(summary['confirmedCasesIndian'])),html.Td(html.H4(summary['discharged'])),html.Td(html.H4(summary['deaths']))]
 
             )])
             
@@ -48,9 +61,20 @@ app.layout = html.Div(id='dark-theme-container',children=[
 
     dcc.Graph(
         id='example-graph',
-        figure=fig
-)])
+        figure=fig,
+),
+html.H1("Record of Cases"),
+html.Div(children=[
+dcc.Dropdown(
+        id='demo-dropdown',
+        options=covid.history_labels_regions(),
+        value=covid.history_labels_regions()[0]['value']
+    )])
+
+
+])
 
 
 if __name__ == '__main__':
+
     app.run_server(debug=True)
