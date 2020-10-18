b3f7d621658d11926541646792a34964546a11ec
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2020-10-19 03:20:32 +0530

    Added trends graph and tabs

--- src/app.py
+++ src/app.py
@@ -6,75 +6,194 @@ import pandas as pd
 from covidCases import CovidCases
 import plotly.graph_objects as go
 import dash_daq as daq
-covid=CovidCases()
-regions=covid.latest_cases()
-summary=covid.summary()
-
-
+from dash.dependencies import Input, Output
+import json
 external_stylesheets = ['https://codepen.io/anon/pen/mardKv.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-theme =  {
+@app.callback(
+    Output(component_id='history_table', component_property='children'),
+    [Input(component_id='region_option', component_property='value'),
+    Input(component_id='date_option', component_property='value')
+    ]
+)
+def update_output_div(state,date):
+    ob=CovidCases()
+    return ob.return_history(state,date)
+
+@app.callback(
+    Output(component_id='cases_graph', component_property='figure'),
+    [Input(component_id='history_graph', component_property='value'),
+    Input(component_id='region_graph', component_property='value'),
+    ])
+
+def update_case_graph(case,region):
+    obj=CovidCases()
+    dictionary=obj.return_history_graph()
+    regions=[]
+    fig = go.Figure()
+    if 'All' in region and type(region)==str:
+            regions=json.loads(open("states.dat").read())
+    elif 'All' in region and type(region)==list:
+            regions=json.loads(open("states.dat").read())
+    elif type(region)==str:
+        regions.append(region)
+    else:
+        regions=region
+
+
+
+    x=[date for date in dictionary.keys()]
+    print(regions)
+    print("region is ",region)
+    for region in regions:
+        try:
+            y=[]
+            for death in dictionary.values():
+                try:
+                    if region in death:
+                        y.append(death[region][case])
+                    else:
+                        y.append(0)
+                except Exception as e:
+                    pass
+            print(len(x),len(y))
+            fig.add_trace(go.Scatter(x=x, y=y,
+                    mode='lines',
+                    name=region))
+            fig.update_traces(marker=dict(size=1,
+                              line=dict(width=2,
+                                        color='DarkSlateGrey')),
+                  )
+            
+        except Exception as e:
+            pass
+            continue
+    return fig
+    
+def tab1_data():
+    theme =  {
     'dark': True,
     'detail': '#007439',
     'primary': '#00EA64',
     'secondary': '#6E6E6E',
-}
-
-df = pd.DataFrame({
-    "States": [i['loc'] for i in regions],
-    "Confirmed Cases (Indian)": [i['confirmedCasesIndian'] for i in regions],
-    "Confirmed Cases (Foreign)": [i['confirmedCasesForeign'] for i in regions],
-    'Discharged'  :[i['discharged'] for i in regions],
-    'Deaths':[i['deaths'] for i in regions],
-})
-
-fig = go.Figure(data=[
-    go.Bar(name='Confirmed Cases-India', x=df['States'], y=df['Confirmed Cases (Indian)']),
-    go.Bar(name='Confirmed Cases-Foreign', x=df['States'], y=df['Confirmed Cases (Foreign)']),
-    go.Bar(name='Deaths', x=df['States'], y=df['Deaths']),
-    go.Bar(name='Discharged', x=df['States'], y=df['Discharged'])
-    
-])
-app.layout = html.Div(id='dark-theme-container',children=[daq.DarkThemeProvider(theme=theme),
-    html.H1(children='Latest Covid-19 Cases'),
+        }
+    covid=CovidCases()
+    regions=covid.latest_cases()
+    summary=covid.summary()
 
     
-    html.Div([
-        html.Table([
-            html.Tr(
-                [html.Td(html.H4('Total')),html.Td(html.H4('Overall Confirmed')),html.Td(html.H4('Overall Discharged')),html.Td(html.H4('Overall Deaths'))]
+    
+    
+    df = pd.DataFrame({
+        "States": [i['loc'] for i in regions],
+        "Confirmed Cases (Indian)": [i['confirmedCasesIndian'] for i in regions],
+        "Confirmed Cases (Foreign)": [i['confirmedCasesForeign'] for i in regions],
+        'Discharged'  :[i['discharged'] for i in regions],
+        'Deaths':[i['deaths'] for i in regions],
+    })
 
-            ),
-            html.Tr(
-                [html.Td(daq.LEDDisplay(
-        value="3.14159",
-        color=theme['primary'],
-        id='darktheme-daq-leddisplay',
-        className='dark-theme-control'
+    fig = go.Figure(data=[
+        go.Bar(name='Confirmed Cases-India', x=df['States'], y=df['Confirmed Cases (Indian)']),
+        go.Bar(name='Confirmed Cases-Foreign', x=df['States'], y=df['Confirmed Cases (Foreign)']),
+        go.Bar(name='Deaths', x=df['States'], y=df['Deaths']),
+        go.Bar(name='Discharged', x=df['States'], y=df['Discharged'])
         
-    )),html.Td(html.H4(summary['confirmedCasesIndian'])),html.Td(html.H4(summary['discharged'])),html.Td(html.H4(summary['deaths']))]
+    ])
+    return html.Div(id='dark-theme-container',children=[daq.DarkThemeProvider(theme=theme),
+        html.H1(children='Latest Covid-19 Cases'),
 
-            )])
+        
+        html.Div([
+            html.Table([
+                html.Tr(
+                    [html.Td(html.H4('Total')),html.Td(html.H4('Overall Confirmed')),html.Td(html.H4('Overall Discharged')),html.Td(html.H4('Overall Deaths'))]
+
+                ),
+                html.Tr(
+                    [html.Td(daq.LEDDisplay(
+            value="3.14159",
+            color=theme['primary'],
+            id='darktheme-daq-leddisplay',
+            className='dark-theme-control'
             
-          
+        )),html.Td(html.H4(summary['confirmedCasesIndian'])),html.Td(html.H4(summary['discharged'])),html.Td(html.H4(summary['deaths']))]
+
+                )])
+                
+            
+        ]),
+
+        dcc.Graph(
+            id='example-graph',
+            figure=fig,
+    ),
+    html.H1("Record of Cases"),
+    html.Div(children=[
+        html.Table(html.Tr([
+            html.Td([html.H3("States"),
+    dcc.Dropdown(
+            id='region_option',
+            options=covid.history_labels_regions(),
+            value='All',
+            multi=True
+        )]),html.Td([html.H3("Date"),
+    dcc.Dropdown(
+            id='date_option',
+            options=covid.history_labels_dates(),
+            value=covid.history_labels_dates()[0]['value'],
+            
+        )]
+
+        
+        )]))
+
+
     ]),
+    html.Div(children=[html.Div(id="history_table")]),
+    html.Div(children=[html.H1("Trends Year Till Date"),html.Div(children=[
+        html.Div(
+        dcc.Dropdown(id="history_graph",
+                        options=[{'label':'Deaths','value':'Deaths'},
+                                {'label':'Confirmed','value':'Confirmed'},
+                                {'label':'Discharged','value':'Discharged'}],
+                        value='Deaths'
+                                ),style={"width":"20%","display":"inline-block"})
+        ,
+        html.Div(
+        dcc.Dropdown(id="region_graph",
+                        options=covid.history_labels_regions(),
+                        value='All',
+                        multi=True
+                                ),style={"width":"20%","display":"inline-block"}),
+        
+        dcc.Graph(id="cases_graph")],style={'width':'100%'})])
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig,
-),
-html.H1("Record of Cases"),
-html.Div(children=[
-dcc.Dropdown(
-        id='demo-dropdown',
-        options=covid.history_labels_regions(),
-        value=covid.history_labels_regions()[0]['value']
-    )])
+    ])
 
 
-])
+@app.callback(Output('tabs-example-content', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div(tab1_data())
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
+
+def initial_load():
+    
+    app.layout =html.Div([
+    dcc.Tabs(id='tabs-example', value='tab-1', children=[
+        dcc.Tab(label='Tab one', value='tab-1'),
+        dcc.Tab(label='Tab two', value='tab-2'),
+    ]),
+    html.Div(id='tabs-example-content')])
+    app.config['suppress_callback_exceptions'] = True
+    app.config.suppress_callback_exceptions = True
+    return app
 
 
 if __name__ == '__main__':
-
+    app=initial_load()
     app.run_server(debug=True)
--- src/covidCases.py
+++ src/covidCases.py
@@ -2,6 +2,8 @@ import requests
 import configparser
 from logger import Logger
 import pandas as pd
+import dash_table
+import json
 class CovidCases:
 	def __init__(self):
 		self.config=configparser.ConfigParser()
@@ -49,14 +51,141 @@ class CovidCases:
 		for i in range(0,region_history.shape[1]-1):
 			states.extend(pd.DataFrame(region_history.iloc[1][i])['loc'].to_list())
 		states=sorted(list(set(states)))
+		states2=json.dumps(states)
+		with open("states.dat","w") as f:
+			f.write(states2)
+		
+
 		dictionary=[]	
 		for state in states:
 			dictionary.append({'label':state,'value':state})
+		dictionary.append({'label':'All','value':'All'})
+		return dictionary
+	def history_labels_dates(self):
+		region_history=self.history()
+		dates=region_history.columns
+		dictionary=[]
+		for date in dates:
+			if date=='day':
+				continue
+			dictionary.append({'label':date,'value':date})
+		
 		return dictionary
 
 
-ob=CovidCases()
-ob.history_labels_regions()
+	def return_history_graph(self):
+		region_history=self.history()
+		region_history['type']=region_history.index
+		dictionary={}
+		df1=region_history.transpose()
+		for indexs in range(0,len(df1)):
+				dd={}
+				try:
+					for day in df1.regional[indexs]:
+						dd[day['loc']]={'Indian':day['confirmedCasesIndian'],'Deaths':day['deaths'],'Discharged':day['discharged'],'Confirmed':day['totalConfirmed']}
+				except:
+							break 
+				dictionary[df1.index[indexs]]=dd	
+		return dictionary
+
+
+
+
+
+	def return_history(self,state,date):
+		region_history=self.history()
+		states=[]
+		
+		
+		if 'All' in state and type(state)!=list:
+
+			states=json.loads(open("states.dat").read())
+
+			state=['All']
+		elif 'All' in state and type(state)==list:
+			 states=json.loads(open("states.dat").read())
+
+
+		elif type(state)==str:
+			states.append(state)
+		else:
+			states=state
+
+		df4=pd.DataFrame(region_history.loc['regional'][date])
+		df5=df4[df4['loc'].isin(states)]
+		df5=df5[['loc', 'confirmedCasesIndian', 'confirmedCasesForeign', 'discharged', 'deaths', 'totalConfirmed']]
+		df5['Death_Percent']=round(df5['deaths']*100/df5['deaths'].sum(),2)
+		df5.Death_Percent.fillna(0,inplace=True)
+		df5.rename(columns={
+			'loc':'State',
+			'confirmedCasesIndian':'Indian',
+			'confirmedCasesForeign':'Foreigner',
+			'discharged':'Discharged',
+			'deaths':'Deaths',
+			'totalConfirmed':'Total Confirmed',
+			
+			},inplace=True)
+		return dash_table.DataTable(
+    		id='table',
+    		columns=[{"name": i, "id": i} for i in df5.columns],
+			sort_action="native",
+        	sort_mode="multi",
+    		data=df5.to_dict('records'),
+			style_cell_conditional=[
+        {
+            'if': {'column_id': c},
+            'textAlign': 'left'
+        } for c in ['Date', 'Region']
+    ],
+    style_data_conditional=[
+        {
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(248, 248, 248)'
+        },
+			{
+            'if': {
+                'column_id': 'Death_Percent',
+                'filter_query': '{Death_Percent} >= 0'
+            },
+            'backgroundColor': 'green',
+			'color':'white'},{
+            'if': {
+                'column_id': 'Death_Percent',
+                'filter_query': '{Death_Percent} > 10'
+            },
+            'backgroundColor': 'lime',
+			'color':'white'},
+				{
+            'if': {
+                'column_id': 'Death_Percent',
+                'filter_query': '{Death_Percent} > 30'
+            },
+            'backgroundColor': 'orange',
+			'color':'white'},
+			{
+            'if': {
+                'column_id': 'Death_Percent',
+                'filter_query': '{Death_Percent} >= 50'
+            },
+            'backgroundColor': 'red',
+			'color':'white'},
+		
+			
+	],       
+    
+    style_header={
+        'backgroundColor': 'rgb(230, 230, 230)',
+        'fontWeight': 'bold'
+    },
+	
+			
+			)
+
+
+
+
+
+
 
 
 
